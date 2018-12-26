f0042a8aa6a7a096cd4eb160f7f0998c9a0fea2a
Jagadeesh04 <jagadeesh.g07@gmail.com>
2018-12-26 17:43:19 -0500

    basic spring web maven project created

---
+++ spring-in-10-steps/src/main/java/com/jag/springboot/basics/springin10steps/SpringIn10StepsApplication.java
@@ -0,0 +1,14 @@
+package com.jag.springboot.basics.springin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn10StepsApplication.class, args);
+	}
+
+}
+
---
+++ spring-in-10-steps/src/test/java/com/jag/springboot/basics/springin10steps/SpringIn10StepsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jag.springboot.basics.springin10steps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringIn10StepsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
